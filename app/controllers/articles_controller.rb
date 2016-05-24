ea76844edf98e063cd591708e0893fc52a89f759
Calmeii <1972703660@qq.com>
2016-05-24 20:43:25 +0800

    update-1.1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,18 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, :only => ['now']
+  layout "articles_layout"
   def index
+    @articles = Article.all
   end
   #显示用户的文章
   def now
     @articles = current_user.articles
   end
 
+  #显示指定文章
+  def show
+    @article = current_user.articles.find(params[:id])
+  end
   #增加新文章
   def new
     @article = Article.new
@@ -18,6 +25,25 @@ class ArticlesController < ApplicationController
       render :action => :new
     end
   end
+  #更改文章
+  def edit
+    @article = current_user.articles.find(params[:id])
+  end
+  def update
+    @article = current_user.articles.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_now_url
+    else
+      render :action => :edit
+    end
+  end
+  #删除文章
+  def destroy
+    @article = current_user.articles.find(params[:id])
+    @article.destroy
+    redirect_to articles_now_url
+  end
+
 
   protected
 
